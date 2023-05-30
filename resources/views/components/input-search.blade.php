5310bcabfceddb633adb983aec19a487c749a320
Jistoper <jon4than1304@gmail.com>
2023-05-30 21:56:29 +0700

    setup

---
+++ resources/views/components/input-search.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <form action="{{ $url }}" method="GET">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="{{ $placeholder }}" value="{{ request()->search }}"
+                name="search">
+            <span class="input-group-append">
+                <button type="button" class="btn btn-dark">
+                    <i class="fas fa-search"></i>
+                </button>
+            </span>
+        </div>
+    </form>
+</div>
