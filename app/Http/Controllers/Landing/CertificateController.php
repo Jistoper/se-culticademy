9a12b0222812db297fa2959b087901d8c8798c50
nhumam123 <naufal.pujianputra@gmail.com>
2023-06-26 22:29:21 +0700

    Adding certificate layout

--- app/Http/Controllers/Landing/CertificateController.php
+++ app/Http/Controllers/Landing/CertificateController.php
@@ -9,6 +9,8 @@ use App\Models\User;
 use Barryvdh\DomPDF\PDF as DomPDFPDF;
 use Barryvdh\DomPDF\PDF;
 use Carbon\Carbon;
+use Dompdf\Dompdf;
+use Dompdf\Options;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
 
@@ -19,8 +21,7 @@ class CertificateController extends Controller
         $user = Auth::user();
         $course = Course::find($courseId);
 
-        // Check is certificate already printed
-
+        // Check if certificate is already printed
 
         $data = $this->getCertificateData($user, $course);
         $pdf = $this->generateCertificatePDF($data);
@@ -40,7 +41,8 @@ class CertificateController extends Controller
             ]);
 
         // Return the created CourseCertification model if needed
-        return $this->downloadCertificate($certificate);
+        // return $this->downloadCertificate($certificate);
+        return redirect()->back()->with('success', 'Certifikat telah dicetak, silahkan klik lihat untuk melihat sertifikat');
     }
 
     public function downloadCertificate($certificateId)
@@ -51,25 +53,25 @@ class CertificateController extends Controller
             ->where('user_id', $user->id)
             ->first();
 
-            $filePath = $certificate->certificate_path;
+        $filePath = $certificate->certificate_path;
 
-            // Check if the file exists
-            if (Storage::exists($filePath)) {
-                // Retrieve the file's name
-                $fileName = basename($filePath);
+        // Check if the file exists
+        if (Storage::exists($filePath)) {
+            // Retrieve the file's name
+            $fileName = basename($filePath);
 
-                // Set the appropriate response headers for file download
-                $headers = [
-                    'Content-Type' => 'application/pdf', // Replace with the appropriate MIME type for your file
-                    'Content-Disposition' => 'attachment; filename="' . $fileName . '"',
-                ];
+            // Set the appropriate response headers for file download
+            $headers = [
+                'Content-Type' => 'application/pdf', // Replace with the appropriate MIME type for your file
+                'Content-Disposition' => 'attachment; filename="' . $fileName . '"',
+            ];
 
-                // Download the file
-                return Storage::download($filePath, $fileName, $headers);
-            }
+            // Download the file
+            return Storage::download($filePath, $fileName, $headers);
+        }
 
-            // File not found
-            abort(404);
+        // File not found
+        abort(404);
     }
 
     private function getCertificateData($user, $course)
@@ -80,93 +82,51 @@ class CertificateController extends Controller
             'date' => date('Y-m-d'),
         ];
     }
-
     private function generateCertificatePDF($data)
     {
-        $pdf = app()->make('dompdf.wrapper');
-        $pdf->loadView('certificate.index', $data);
-        $pdf->setPaper('A4', 'landscape');
-        return $pdf;
+        // Create a new Dompdf instance
+        $dompdf = new Dompdf();
+
+        // Load the HTML template for the certificate
+        $html = view('certificate.index', $data)->render();
+
+        // Set the paper size and orientation
+        $dompdf->setPaper('A4', 'landscape');
+
+        // Get the default options
+        $options = new Options();
+        $options->set('isRemoteEnabled', true);
+        $options->set('isFontSubsettingEnabled', true);
+
+        // Specify the font families
+        $options->set('fontDir', public_path('fonts/'));
+        $options->set('defaultFont', 'Arial');
+        $options->set('fontFamily', 'Arial, PinyonScript'); // Multiple font families separated by comma
+        $options->setFontFa
+        // Set the options for the Dompdf instance
+        $dompdf->setOptions($options);
+
+        // Load the HTML content into dompdf
+        $dompdf->loadHtml($html);
+
+        // Render the PDF
+        $dompdf->render();
+
+        // Return the generated PDF
+        return $dompdf;
     }
-    // public function generateCertificatex($userId, $courseId)
-    // {
-    //     $user = User::find($userId);
-    //     $course = Course::find($courseId);
 
-    //     $data = $this->getCertificateData($user, $course);
-    //     $pdf = $this->generateCertificatePDF($data);
 
-    //     return $pdf->download('certificate.pdf');
-    // }
 
-    // public function generateCertificate($userId, $courseId)
-    // {
-    //     $user = User::find($userId);
-    //     $course = Course::find($courseId);
+    public function testPage($userId, $courseId)
+    {
+        $user = User::find($userId);
+        $course = Course::find($courseId);
 
-    //     $data = $this->getCertificateData($user, $course);
-    //     $pdf = $this->generateCertificatePDF($data);
+        $name = $user->name;
+        $course = $course->name;
+        $date = now();
 
-    //     // Save the PDF to storage/app/public/certificates directory
-    //     $path = 'certificates/';
-    //     $filename = $course->id . '_' . $user->id . '_' . Carbon::now()->format('YmdHis') . '.pdf';
-    //     Storage::put($path . $filename, $pdf->output());
-
-    //     // Update the course_certifications table with the file path
-    //     $certificate = CourseCertification::create([
-    //         'course_id' => $course->id,
-    //         'user_id' => $user->id,
-    //         'certificate_path' => $path . $filename,
-    //     ]);
-
-    //     // Return the created CourseCertification model if needed
-    //     return $certificate;
-    // }
-
+        return view('certificate.index', compact('name', 'course', 'date'));
+    }
 }
-
-     // public function generateCertificate($userId, $courseId)
-    // {
-
-    //     // Retrieve the user and course data from your database
-    //     $user = User::find($userId);
-    //     $course = Course::find($courseId);
-
-    //     // Storage location
-    //     $outputfile = storage_path('app/public/certificate/' . $user . '_' . $course . '.pdf');
-    //     $this->fillPDF(storage_path('app/public/certificate/master/' . $user . '_' . $course . '.pdf'), $outputfile, $user);
-
-    //     return response()->file($outputfile);
-    //     // // Pass the data to the certificate template
-    //     // $data = [
-    //     //     'name' => $user->name,
-    //     //     'course' => $course->name,
-    //     //     'date' => date('F j, Y'),
-    //     // ];
-
-    //     // // Generate the PDF certificate using Dompdf
-    //     // $pdf = \PDF::loadView('certificate', $data)->setPaper('landscape');
-
-    //     // // Return the PDF certificate as a download
-    //     // return $pdf->download('certificate.pdf');
-    // }
-
-    // public function generateCertificate() {
-    //     // create new FPDF instance
-    //     $pdf = new FPDF();
-
-    //     // add a page
-    //     $pdf->AddPage();
-
-    //     // set the font
-    //     $pdf->SetFont('Arial', 'B', 16);
-
-    //     // set the text color to black
-    //     $pdf->SetTextColor(0, 0, 0);
-
-    //     // write the certificate text    $pdf->Cell(0, 10, "Certificate of Completion", 0, 1, 'C');
-    //     $pdf->Cell(0, 10, "This certificate is awarded to $name", 0, 1, 'C');
-
-    //     // output the PDF
-    //     $pdf->Output();
-    // }
